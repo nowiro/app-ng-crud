34fa26140ae6d855a4aeaafb5111ede14b5c1db6
nowiro <nowiro@gmail.com>
2019-11-09 12:36:21 +0100

    lang

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  defaultLanguage: 'pl-PL',
+  supportedLanguages: ['en-US', 'pl-PL']
 };
 
 /*
