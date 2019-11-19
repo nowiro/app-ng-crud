1d309f318ef1a6476d7cd44e84acc60561af3ae7
nowiro <nowiro@gmail.com>
2019-11-19 11:45:38 +0100

    update

--- app/README.md
+++
@@ -1,8 +0,0 @@
-npm install -g yarn
-#Angular CLI
-yarn global add @angular/cli 
-#Angular CLI
-yarn global add webpack-cli
-#npm-check-updates
-yarn global add npm-check-updates
-#If you have issue with ncu use: Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
---
+++ install.sh
@@ -0,0 +1,11 @@
+npm install -g yarn
+#Angular CLI
+yarn global add @angular/cli 
+#Angular CLI
+yarn global add webpack-cli
+#npm-check-updates
+yarn global add npm-check-updates
+#If you have issue with ncu use: Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+#TypeScript
+yarn global add typescript
